5a7c23b8b4ed8ec8f4dc7222e3561a9915d25f7b
mztakeyocrackers-lang <mztakeyocrackers@gmail.com>
2026-04-04 04:24:24 -0500

    Update getrecords.js

--- netlify/functions/getrecords.js
+++ netlify/functions/getrecords.js
@@ -3,15 +3,20 @@ exports.handler = async function(event) {
   const scriptUrl = 'https://script.google.com/macros/s/AKfycbxl3jpAd7DT8iMLokwgcLlNMAHX6WhJFCvdvmWKrTTOneNWPZcfBb7jv6t1CAAvDmEJ/exec?type=' + type;
 
   try {
-    const response = await fetch(scriptUrl);
+    const response = await fetch(scriptUrl, {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0'
+      }
+    });
     const text = await response.text();
+    console.log('Response:', text.substring(0, 200));
     const data = JSON.parse(text);
     return {
       statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
     };
   } catch(e) {
