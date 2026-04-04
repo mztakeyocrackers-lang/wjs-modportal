b3dcc094226c5f74531a4768e407fbb49cd24b50
mztakeyocrackers-lang <mztakeyocrackers@gmail.com>
2026-04-04 04:21:46 -0500

    Update getrecords.js

--- netlify/functions/getrecords.js
+++ netlify/functions/getrecords.js
@@ -1,38 +1,24 @@
-const https = require('https');
-
 exports.handler = async function(event) {
-  const type = event.queryStringParameters && event.queryStringParameters.type ? event.queryStringParameters.type : 'vtos';
+  const type = (event.queryStringParameters && event.queryStringParameters.type) ? event.queryStringParameters.type : 'vtos';
   const scriptUrl = 'https://script.google.com/macros/s/AKfycbxl3jpAd7DT8iMLokwgcLlNMAHX6WhJFCvdvmWKrTTOneNWPZcfBb7jv6t1CAAvDmEJ/exec?type=' + type;
-  
-  return new Promise((resolve) => {
-    https.get(scriptUrl, (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        try {
-          const parsed = JSON.parse(data);
-          resolve({
-            statusCode: 200,
-            headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(parsed)
-          });
-        } catch(e) {
-          resolve({
-            statusCode: 200,
-            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-            body: JSON.stringify({ result: 'error', entries: [] })
-          });
-        }
-      });
-    }).on('error', () => {
-      resolve({
-        statusCode: 200,
-        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-        body: JSON.stringify({ result: 'error', entries: [] })
-      });
-    });
-  });
+
+  try {
+    const response = await fetch(scriptUrl);
+    const text = await response.text();
+    const data = JSON.parse(text);
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch(e) {
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+      body: JSON.stringify({ result: 'error', entries: [], message: e.toString() })
+    };
+  }
 };
